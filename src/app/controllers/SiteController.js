d75a2e5507c81561434e6c89cc9d37df63f966a9
Thang Nguyen <nguyenducthangg30@gmail.com>
2024-09-08 18:51:43 +0700

    update

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,16 +1,20 @@
+const Course = require('../models/Course');
 
 class SiteController {
-    
-    // [GET] / 
-    index (req, res) {
-        res.render('home')
-    }
+  // [GET] /
+  index(req, res, next) {
+    Course.find({})
+      .lean()
+      .then((courses) => {
+        res.render('home', { courses });
+      })
+      .catch(next);
+  }
 
-    // [GET] /search
-    search(req, res) {
-        res.render('search')
-    }
-
+  // [GET] /search
+  search(req, res) {
+    res.render('search');
+  }
 }
 
-module.exports = new SiteController
+module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -1,32 +1,51 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars').engine
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+var methodOverride = require('method-override');
+const handlebars = require('express-handlebars').engine;
 
-const route = require('./routes')
+const SortMiddleware = require('./app/middlewares/SortMiddleware');
 
-app.use(express.static(path.join(__dirname, 'public')))
+const route = require('./routes');
+const db = require('./config/db');
 
-app.use(express.urlencoded({
-  extended: true
-}))
-app.use(express.json())
+// Connet to DB
+db.connect();
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+app.use(methodOverride('_method'));
+
+//Custom middleware
+app.use(SortMiddleware);
 
 //HTTP logger
 // app.use(morgan('combined'))
 
-//Template engine 
-app.engine('hbs', handlebars({
-  extname: ".hbs"
-}))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+//Template engine
+app.engine(
+  'hbs',
+  handlebars({
+    extname: '.hbs',
+    helpers: require('./helpers/handlebars'),
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 //Route init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
